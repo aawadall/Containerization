bdc11277e0dd6693852b68ea10c259f0da0baa3d
aawadall <aawadall@ualberta.ca>
2019-08-03 14:48:23 -0400

    tests are behaving as expected

---
+++ js-ms4/test/controllers/auth.controller.spec.js
@@ -0,0 +1,48 @@
+const assert = require('assert');
+const authController = require('../../controllers/auth.controller'); 
+
+describe('AuthController', function () {
+    beforeEach('Setup user roles',function settingUpRoles() {
+        console.log("Running before each");
+        authController.setRoles(['user']);
+        //throw({error: 'some error'});
+    })
+    describe('isAuthorized', function () {
+        
+        it('Should return false if not authorized', function(){
+            //authController.setRoles(['user']);
+            assert.equal(false, 
+                authController.isAuthorized( 'admin'));
+        })
+
+        it('Should return true if authorized', function(){
+            authController.setRoles(['user','admin']);
+            assert.equal(true, 
+                authController.isAuthorized( 'admin'));
+        })
+    })
+    
+    describe('isAuthorizedAsync',  () => {
+        it('Should return false if not authorized', function(done) {
+            
+            this.timeout(2500);
+            authController.isAuthorizedAsync(
+                    'admin', 
+                    (isAuth) =>{
+                        assert.equal(false, isAuth);
+                        done();
+                });
+            })
+
+        it('Should return true if  authorized', function(done) {
+            authController.setRoles(['user','special']);
+            this.timeout(5000);
+            authController.isAuthorizedAsync(                    
+                    'special', 
+                    (isAuth) =>{
+                        assert.equal(true, isAuth);
+                        done();
+                });
+            })    
+    })
+});
---
+++ js-ms4/test/starting.spec.js
@@ -0,0 +1,7 @@
+const assert = require('assert');
+
+describe('Basic Mocha Test', function () {
+   it('should throw some errors', function() {
+       
+   });
+});
