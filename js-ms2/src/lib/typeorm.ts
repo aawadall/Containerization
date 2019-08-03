8c6e5358ac374546a419a0807888254b76d647d3
aawadall <aawadall@ualberta.ca>
2019-08-02 21:19:19 -0400

    removed failed attempts

--- js-ms2/src/lib/typeorm.ts
+++
@@ -1,6 +0,0 @@
-import { getConnectionOptions, createConnection, Connection } from "typeorm";
-
-export const createTypeormConn = async (): Promise<Connection> => {
-    const connectionOptions = await getConnectionOptions(process.env.NODE_ENV);
-    return createConnection({ ...connectionOptions, name: "default" });
-};
